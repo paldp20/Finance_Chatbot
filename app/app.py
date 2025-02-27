1312896c86d8d710a63184b01c160d8cdf7180db
Pal Patel <144155854+paldp20@users.noreply.github.com>
2025-02-27 17:35:53 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -22,40 +22,6 @@ with st.sidebar:
     add_vertical_space(5)
     st.write('Running in Docker!')
 
-# from langchain_core.prompts import PromptTemplate, FewShotPromptTemplate  # Changed
-# from langchain_core.example_selectors import LengthBasedExampleSelector  # Changed
-# from langchain_openai import OpenAI  # Changed
-# from langchain_community.utilities import SQLDatabase  # Changed
-# from langchain_community.agent_toolkits import SQLDatabaseToolkit  # Changed
-# from langchain_community.agent_toolkits.sql.base import create_sql_agent
-# from langchain.agents.agent_types import AgentType
-
-# # Database credentials
-# DB_USER = 'my_user'
-# DB_PASSWORD = 'my_pass'
-# DB_NAME = 'tickers_db'
-# DB_HOST = 'db'
-# DB_PORT = 3306
-# # mysql_uri = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-# mysql_uri = f"mysql+mysqlconnector://my_user:my_pass@localhost:3306/tickers_db"
-
-# # Initialize the SQLDatabase and SQLDatabaseToolkit
-# db = SQLDatabase.from_uri(mysql_uri)
-# toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0))
-
-# # Create SQL agent
-# agent_executor = create_sql_agent(
-#     llm=OpenAI(temperature=0),
-#     toolkit=toolkit,
-#     verbose=True,
-#     agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-#     handle_parsing_errors=True
-# )
-
-# # Modified the generate_response function to now use the SQL agent
-# def query_db(prompt):
-#     return agent_executor.run(prompt)
-
 # Generate empty lists for generated and past.
 ## generated stores AI generated responses
 if 'generated' not in st.session_state:
